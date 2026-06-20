c04f7de0250684db816506912dfe0fbf9f6cb55e
drac1283 <gabrielcb.1283@gmail.com>
2026-06-20 19:42:16 +0200

    correction d'erreur du fichiers R209-P3-installateur.ps1

--- R209-P3-installateur.ps1
+++ R209-P3-installateur.ps1
@@ -4,7 +4,7 @@ function prompt { "> " }
 Set-Location $env:USERPROFILE\Documents
 $basePath = "$env:USERPROFILE\Documents\SAMBA SAE2.04 Partie 2"
 $sourceDir = $PSScriptRoot # Le dossier où se trouve ce script (souvent "Téléchargements")
-$zipFile = "$sourceDir\fichierconf-docker.zip"
+$zipFile = "$sourceDir\fichiersconf-docker.zip"
 
 if (-not (Test-Path $zipFile)) {
     Write-Host "ERREUR : Le fichier $zipFile est introuvable." -ForegroundColor Red
@@ -20,14 +20,6 @@ Write-Host "Preparation de l'envirronement dees conteneurs" -ForegroundColor Cya
 Write-Host "    -creation du dossier du project" -ForegroundColor Gray
 New-Item -Path $basePath -ItemType Directory -Force | Out-Null
 
-Write-Host "    -Extraction des fichiers necessaires" -ForegroundColor Gray
-Expand-Archive -Path $zipFile -DestinationPath $basePath -Forcev | Out-Null
-Set-Location $basePath
-
-Write-Host "    -nettoyage de l'ancien environnement Docker si deja créer" -ForegroundColor Gray
-docker compose down > $null 2>&1
-
-
 Write-Host "    -creation de l'arborescence des volumes partagé et des backups" -ForegroundColor Gray
 New-Item -Path "$basePath\server-SAMBA\samba_dperson\configuration_backup",
                 "$basePath\server-SAMBA\samba_dperson\partage_stockage\amoi",
@@ -36,15 +28,27 @@ New-Item -Path "$basePath\server-SAMBA\samba_dperson\configuration_backup",
                 "$basePath\server-SAMBA\samba_dperson\partage_stockage\public",
                 "$basePath\server-SAMBA\samba_dperson\partage_stockage\amoi-anous",
                 "$basePath\server-SAMBA\samba_dperson\partage_stockage\amoi-atoi",
-                "$basePath\ubuntu-test" -ItemType Directory -Force | Out-Null
+                "$basePath\ubuntu-test" -ItemType Directory -Force | Out-Null 
 
-Move-Item -Path "$basePath\Dockerfile.samba" -Destination "$basePath\server-SAMBA\samba_dperson"   
-Move-Item -Path "$basePath\Dockerfile.ubuntutest" -Destination "$basePath\ubuntu-test"   
+Write-Host "    -Extraction des fichiers necessaires" -ForegroundColor Gray
+Expand-Archive -Path $zipFile -DestinationPath $basePath -Force | Out-Null
+Move-Item -Path "$basePath\fichiersconf-docker\docker-compose.yml" -Destination $basePath  
+Move-Item -Path "$basePath\fichiersconf-docker\Dockerfile.samba" -Destination "$basePath\server-SAMBA\samba_dperson"   
+Move-Item -Path "$basePath\fichiersconf-docker\Dockerfile.ubuntutest" -Destination "$basePath\ubuntu-test"   
 
+Set-Location $basePath
+
+
+
+Write-Host "    -nettoyage de l'ancien environnement Docker si deja créer" -ForegroundColor Gray
+docker compose down > $null 2>&1
 
 Write-Host "Création des images et des conteneurs" -ForegroundColor Cyan
 Set-Location "$basePath"
-docker compose up -d --build > $null 2>&1
+docker compose up -d --build
+
+Write-Host "supression des poussiere" -ForegroundColor Cyan
+Remove-Item "$basePath\fichiersconf-docker" -Recurse -Force > $null 2>&1
 
 Write-Host ""
 Write-Host "=== Installation terminée avec succès ! ===" -ForegroundColor Cyan
